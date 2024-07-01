0562c5d0904fe78f4164b4276a415cffc3ae1a2b
mcc9513 <58860627+mcc9513@users.noreply.github.com>
2024-07-01 11:07:20 -0400

    Update debug.js

--- Day 25/debug.js
+++ Day 25/debug.js
@@ -27,9 +27,11 @@ const storeOwners = [
     totalLocations += storeOwners[i].stores;
     }
     return totalLocations;
+      // return i was changed to return total locations because the value storeOwners[i].stores is assigned to totalLocations 
   };
   
   let locations = listNumberOfStores();
+// parenthisis were added to listNumberOfStores to assign the function to the variable "locations"
   
   function tellMeMyStores() {
     console.log('Hey, can you tell me how many stores you have?');
@@ -42,8 +44,10 @@ const storeOwners = [
     for (let i = 0; i < storeOwners.length; i++) {
       let person = storeOwners[i].name;
       let location = storeOwners[i].location;
+        // [i] was added to storeOwners in both lines above to be sure that the for loop was referenced properly
       console.log('Yes, ' + person + ' has one in ' + location);
     }
+      //curly brackets were removed from around the objects to ensure they were called as objects, if the curly brackets remained, a dollar sign would need to be added ex. ${person} and then the plus sign would not be necessary
   
   }
   
